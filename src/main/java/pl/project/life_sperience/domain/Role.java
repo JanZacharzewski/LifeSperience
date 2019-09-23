6862f1fa13607f3c976900f721211cbf8060f055
Jan Zacharzewski <janzacharzewski4@gmail.com>
2019-09-23 20:41:31 +0200

    poprawki

---
+++ src/main/java/pl/project/life_sperience/domain/Role.java
@@ -0,0 +1,18 @@
+package pl.project.life_sperience.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
